b4f8f952156d55b2505b7632ddf906fb3f535b5d
Bruno Romas <bpromas@gmail.com>
2026-05-19 18:36:19 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://plans-42.vercel.app'],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
-  
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
